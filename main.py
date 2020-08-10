d95faacc88005ccab782b1ed627d73a38e9167b7
varun-wahi <69451736+varun-wahi@users.noreply.github.com>
2020-08-10 14:24:21 +0530

    Added Font size and Position

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.core.window import Window
 
-Window.clearcolor = (0 / 255, 0 / 255, 0 / 255, 1) #Set the Color of the Background
+Window.clearcolor = (0 / 255, 0 / 255, 0 / 255, 1) #Set the Color of the Background (Optional)
 
 
 class FirstApp(App): # Initiate MyApp Class and Inherit from App Class(inbuilt)
@@ -15,9 +15,13 @@ class FirstApp(App): # Initiate MyApp Class and Inherit from App Class(inbuilt)
                       color=(0, 0, 0, 1),
                       bold=True
                       )
+
         button1 = Button(text="exit",
                          color=(1, 1, 1,1),
-                         size_hint=(0.4,0.5)
+                         size_hint=(0.5,0.2),
+                         font_size=30,
+                         pos_hint= {'center_x':0.5,'center_y':0.5} #Center of the Screen
+
                          )
         return button1
 
